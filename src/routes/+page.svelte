a568ea90b02b4aaf2ebf64ae4f86410943a3569e
Jeffrey Li <jeffrey@vectorwyse.com>
2023-05-05 19:25:43 -0400

    add buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,49 @@
 <script lang="ts">
-	let opacity = 75;
+	let opacity = 100;
+
+	const min = 60;
+	const max = 100;
+	const step = 5;
+
+	function decrease() {
+		const newOpacity = opacity - step;
+		opacity = newOpacity < min ? min : newOpacity;
+	}
+
+	function increase() {
+		const newOpacity = opacity + step;
+		opacity = newOpacity > max ? max : newOpacity;
+	}
 </script>
 
 <div class="container">
-	<input bind:value={opacity} class="slider" type="range" min="60" max="100" />
 	<div class="black" style={`--opacity: ${opacity / 100};`}>
 		<div class="text">
 			<p>Contrast</p>
 			<p>對比</p>
 		</div>
 	</div>
+	<div class="controls">
+		<input on:click={decrease} class="button" type="button" value="-" />
+		<input bind:value={opacity} class="slider" type="range" {min} {max} />
+		<input on:click={increase} class="button" type="button" value="+" />
+	</div>
 </div>
 
 <style>
 	.container {
-		padding: 10px;
+		padding: 1rem;
 	}
 
 	.slider {
-		width: 100%;
+		flex: 1 1 auto;
+		margin: 0 1rem 0 1rem;
 	}
 
 	.black {
 		position: relative;
 		width: 100%;
-		height: 200px;
-		margin-top: 10px;
+		height: 25rem;
 		background-color: rgba(0, 0, 0, var(--opacity));
 	}
 
@@ -35,12 +53,23 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-		font-size: 60px;
+		font-size: 5rem;
 		text-transform: uppercase;
 	}
 
 	.text p {
 		text-align: center;
 		line-height: 0;
+	}
+
+	.controls {
+		margin-top: 0.5rem;
+		display: flex;
+	}
+
+	.button {
+		flex: none;
+		width: 2.5rem;
+		height: 2.5rem;
 	}
 </style>
